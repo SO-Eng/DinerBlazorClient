d08c859578cfebc6607dbe7ce6c2585a399390ca
SoftwOrt Engineering <markus.sczeburek-ort@softwort-engineering.com>
2020-05-07 16:28:01 +0200

    Added Update DB (Order) to Display

--- DinerBlazorClientApp/Models/OrderUpdateModel.cs
+++ DinerBlazorClientApp/Models/OrderUpdateModel.cs
@@ -1,13 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace DinerBlazorClientApp.Models
 {
     public class OrderUpdateModel
     {
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20, ErrorMessage = "You need to keep the name to max of 20 Characters")]
+        [MinLength(3, ErrorMessage = "You need to enter at least 3 characters for an order name")]
+        [DisplayName("Name for the Order")]
         public string OrderName { get; set; }
 
     }
