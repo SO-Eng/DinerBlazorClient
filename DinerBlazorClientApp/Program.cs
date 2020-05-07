a00eb94e4d35792da829022a8e53151050e36e53
SoftwOrt Engineering <markus.sczeburek-ort@softwort-engineering.com>
2020-05-07 09:35:23 +0200

    Prepared for API Address to communicate with

--- DinerBlazorClientApp/Program.cs
+++ DinerBlazorClientApp/Program.cs
@@ -1,12 +1,8 @@
 using System;
 using System.Net.Http;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace DinerBlazorClientApp
 {
@@ -17,7 +13,12 @@ namespace DinerBlazorClientApp
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddTransient(sp => new HttpClient
+            {
+                // TODO: Change this to correct address!
+                //BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+                BaseAddress = new Uri("https://localhost:44538")
+            });
 
             await builder.Build().RunAsync();
         }
