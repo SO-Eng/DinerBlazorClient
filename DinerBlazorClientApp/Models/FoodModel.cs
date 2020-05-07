3f4a15c0be4fa84756b3e9a4211d795ac30ab19c
SoftwOrt Engineering <markus.sczeburek-ort@softwort-engineering.com>
2020-05-07 16:01:00 +0200

    Added Create Page for Order
    Added also Models that have to be
    initialized, changed NavMenu and
    set using statement for Models.

---
+++ DinerBlazorClientApp/Models/FoodModel.cs
@@ -0,0 +1,11 @@
+﻿namespace DinerBlazorClientApp.Models
+{
+    public class FoodModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
---
+++ DinerBlazorClientApp/Models/OrderUpdateModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DinerBlazorClientApp.Models
+{
+    public class OrderUpdateModel
+    {
+        public int Id { get; set; }
+        public string OrderName { get; set; }
+
+    }
+}
